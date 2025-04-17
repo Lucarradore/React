b842566e518249875901696e6bd5576e5e2058fd
Lucrecia <lucrecarradore@gmail.com>
2025-04-17 15:26:21 -0300

    React

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
 import navbarReducer from "./navbarSlice";
 
 export const store = configureStore({
